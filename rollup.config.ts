51b3e54017c1175deb82e5d62829b927aff0a922
maksimk <maksim@fuse8.ru>
2018-06-04 11:43:28 +0500

    build: rename library

--- rollup.config.ts
+++ rollup.config.ts
@@ -7,7 +7,7 @@ import json from "rollup-plugin-json";
 
 const pkg = require("./package.json");
 
-const libraryName = "tfsm";
+const libraryName = "ts-fsm";
 
 export default {
 	input: `src/${libraryName}.ts`,
